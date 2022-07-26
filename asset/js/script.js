231b631b1a9ae27f8d2a7015edbd4f363356db3e
aklimaruhina <aklimacse10@gmail.com>
2022-07-26 23:36:40 +0600

    commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -118,3 +118,28 @@ $(window).scroll(function(){
      document.body.style.paddingTop = '0';
    }
 });
+$(".testimonial-reel").slick({
+  centerMode: true,
+  centerPadding: "40px",
+  dots: true,
+  slidesToShow: 3,
+  infinite: true,
+  arrows: false,
+  lazyLoad: "ondemand",
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        centerMode: false
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
